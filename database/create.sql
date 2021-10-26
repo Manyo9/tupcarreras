3a1dadacd0b172007367347c4cf06d52dac6ae4a
Manyo <32688128+Manyo9@users.noreply.github.com>
2021-10-26 20:54:42 -0300

    Arreglo un par de tipos de dato

--- database/create.sql
+++ database/create.sql
@@ -5,14 +5,15 @@ go
 
 create table Carreras
 (id_carrera int identity,
-nombre_titulo varchar(60)
+nombre_titulo varchar(70),
+anio_maximo tinyint,
 constraint pk_carrera primary key (id_carrera)
 )
 
 create table Asignaturas
 (
 id_asignatura int identity,
-nombre varchar(40)
+nombre varchar(60)
 constraint pk_asignaturas primary key (id_asignatura)
 )
 
@@ -21,7 +22,7 @@ create table Detalles_carrera
 id_detalle int identity,
 id_carrera int,
 anio_cursado tinyint,
-cuatrimestre char(1),
+cuatrimestre varchar(1),
 id_asignatura int
 constraint pk_detalle_carrera primary key (id_detalle),
 constraint fk_carrera_detalle foreign key (id_carrera)
