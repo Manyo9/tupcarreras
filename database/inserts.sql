6b3b74a3c425f0f37a03bee755f128ce428fe149
PMonticoli <1134191@tecnicatura.frc.utn.edu.ar>
2021-10-26 21:04:28 -0300

    Agrego inserts primera carrera

---
+++ database/inserts.sql
@@ -0,0 +1,109 @@
+insert into Carreras (nombre_titulo)
+values ('Tecnicatura Universitaria en Programacion')
+
+--INSERT Detalle_carrera Materias 1°er Cuatrimestre 1er año
+insert into Detalles_carrera(id_carrera,anio_cursado,cuatrimestre,id_asignatura)
+values(1,1,'P',1)
+insert into Detalles_carrera(id_carrera,anio_cursado,cuatrimestre,id_asignatura)
+values(1,1,'P',2)
+insert into Detalles_carrera(id_carrera,anio_cursado,cuatrimestre,id_asignatura)
+values(1,1,'P',3)
+insert into Detalles_carrera(id_carrera,anio_cursado,cuatrimestre,id_asignatura)
+values(1,1,'P',4)
+insert into Detalles_carrera(id_carrera,anio_cursado,cuatrimestre,id_asignatura)
+values(1,1,'P',5)
+
+--INSERT Detalle_carrera Materias 2°do Cuatrimestre 1er año
+insert into Detalles_carrera(id_carrera,anio_cursado,cuatrimestre,id_asignatura)
+values(1,1,'S',6)
+insert into Detalles_carrera(id_carrera,anio_cursado,cuatrimestre,id_asignatura)
+values(1,1,'S',7)
+insert into Detalles_carrera(id_carrera,anio_cursado,cuatrimestre,id_asignatura)
+values(1,1,'S',8)
+insert into Detalles_carrera(id_carrera,anio_cursado,cuatrimestre,id_asignatura)
+values(1,1,'S',9)
+insert into Detalles_carrera(id_carrera,anio_cursado,cuatrimestre,id_asignatura)
+values(1,1,'S',10)
+insert into Detalles_carrera(id_carrera,anio_cursado,cuatrimestre,id_asignatura)
+values(1,1,'S',11)
+
+--INSERT DE ASGINATURA 1º Cuatrimestre
+insert into Asignaturas (nombre)
+values ('Sistemas de Procesamiento de Datos')
+insert into Asignaturas (nombre)
+values ('Matemática')
+insert into Asignaturas (nombre)
+values ('Inglés I')
+insert into Asignaturas (nombre)
+values ('Sistemas de Procesamiento de Datos')
+insert into Asignaturas (nombre)
+values ('Laboratorio de Computación I')
+
+
+--INSERT DE ASGINATURA 2º Cuatrimestre
+insert into Asignaturas (nombre)
+values ('Programación II')
+insert into Asignaturas (nombre)
+values ('Arquitectura y Sistemas Operativos')
+insert into Asignaturas (nombre)
+values ('Estadística')
+insert into Asignaturas (nombre)
+values ('Inglés II')
+insert into Asignaturas (nombre)
+values ('Laboratorio de Computación II')
+insert into Asignaturas (nombre)
+values ('Metodología de la Investigación')
+
+
+
+----------------SEGUNDO AÑO-----------------------------
+
+
+
+--INSERT DE ASGINATURA 1º Cuatrimestre 2do año
+insert into Asignaturas (nombre)
+values ('Programación III')
+insert into Asignaturas (nombre)
+values ('Organización Contable de la Empresa')
+insert into Asignaturas (nombre)
+values ('Organización Empresarial')
+insert into Asignaturas (nombre)
+values ('Elementos de Investigación Operativa')
+insert into Asignaturas (nombre)
+values ('Laboratorio de Computación III')
+
+
+--INSERT DE ASGINATURA 2º Cuatrimestre 2do año
+insert into Asignaturas (nombre)
+values ('Metodología de Sistemas I')
+insert into Asignaturas (nombre)
+values ('Diseño y Administración de Bases de Datos')
+insert into Asignaturas (nombre)
+values ('Legislación')
+insert into Asignaturas (nombre)
+values ('Laboratorio de Computación IV')
+
+
+--INSERT Detalle_carrera Materias 1°er Cuatrimestre 2do año
+insert into Detalles_carrera(id_carrera,anio_cursado,cuatrimestre,id_asignatura)
+values(1,2,'P',12)
+insert into Detalles_carrera(id_carrera,anio_cursado,cuatrimestre,id_asignatura)
+values(1,2,'P',13)
+insert into Detalles_carrera(id_carrera,anio_cursado,cuatrimestre,id_asignatura)
+values(1,2,'P',14)
+insert into Detalles_carrera(id_carrera,anio_cursado,cuatrimestre,id_asignatura)
+values(1,2,'P',15)
+insert into Detalles_carrera(id_carrera,anio_cursado,cuatrimestre,id_asignatura)
+values(1,2,'P',16)
+
+--INSERT Detalle_carrera Materias 2°do Cuatrimestre 2do año
+
+insert into Detalles_carrera(id_carrera,anio_cursado,cuatrimestre,id_asignatura)
+values(1,2,'S',17)
+insert into Detalles_carrera(id_carrera,anio_cursado,cuatrimestre,id_asignatura)
+values(1,2,'S',18)
+insert into Detalles_carrera(id_carrera,anio_cursado,cuatrimestre,id_asignatura)
+values(1,2,'S',19)
+insert into Detalles_carrera(id_carrera,anio_cursado,cuatrimestre,id_asignatura)
+values(1,2,'S',20)
+
