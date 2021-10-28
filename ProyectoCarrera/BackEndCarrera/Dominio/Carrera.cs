6c82925ad75d2af71844cf803a3938286f66a681
PMonticoli <1134191@tecnicatura.frc.utn.edu.ar>
2021-10-28 18:56:31 -0300

    Agrego estructura del proyecto

---
+++ ProyectoCarrera/BackEndCarrera/Dominio/Carrera.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEndCarrera.Dominio
+{
+    public class Carrera
+    {
+        public string Nombre { get; set; }
+        public string Titulo { get; set; }
+        public int AnioMaximo { get; set; }
+        public List<DetalleCarrera> Detalles { get; set; }
+        public Carrera()
+        {
+            Detalles = new List<DetalleCarrera>();
+        }
+    }
+}
---
+++ ProyectoCarrera/BackEndCarrera/Dominio/DetalleCarrera.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEndCarrera.Dominio
+{
+    public class DetalleCarrera
+    {
+        public int AnioDeCursado { get; set; }
+        public string Cuatrimestre { get; set; }
+
+        public Asignatura Materia { get; set; }
+        public DetalleCarrera()
+        {
+
+        }
+    }
+}
