2d6bd9bb4791197b1b74f1f961bd5500b4e18912
PMonticoli <1134191@tecnicatura.frc.utn.edu.ar>
2021-10-31 12:18:03 -0300

    Agrego IService y CarreraService

--- ProyectoCarrera/BackEndCarrera/Servicios/CarreraService.cs
+++ ProyectoCarrera/BackEndCarrera/Servicios/CarreraService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BackEndCarrera.Acceso_a_Datos.Implementaciones;
+using BackEndCarrera.Dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,53 @@ using System.Threading.Tasks;
 
 namespace BackEndCarrera.Servicios
 {
-    class CarreraService
+    class CarreraService : IService
     {
+        private CarreraDao dao;
+
+        public CarreraService()
+        {
+            dao = new CarreraDao();
+        }
+
+        public bool ActualizarAsignatura(Asignatura oAsignatura)
+        {
+            return dao.UpdateAsignatura(oAsignatura);
+        }
+
+        public bool ActualizarCarrera(Carrera oCarrera)
+        {
+            return dao.UpdateCarreras(oCarrera);
+        }
+
+        public Carrera BuscarCarreraPorId()
+        {
+            return dao.GetCarrerasById();
+        }
+
+        public bool EliminarCarrera(Carrera oCarrera)
+        {
+            return dao.DeleteCarreras(oCarrera);
+        }
+
+        public bool EliminarDetalle(List<int> lst)
+        {
+            return dao.DeleteDetalle(lst);
+        }
+
+        public bool GuardarAsignatura(Asignatura oAsignatura)
+        {
+            return dao.SaveAsignatura(oAsignatura);
+        }
+
+        public bool GuardarCarrera(Carrera oCarrera)
+        {
+            return dao.SaveCarrera(oCarrera);
+        }
+
+        public List<Asignatura> ObtenerAsignatura()
+        {
+            return dao.GetAsignatura();
+        }
     }
 }
--- ProyectoCarrera/BackEndCarrera/Servicios/IService.cs
+++ ProyectoCarrera/BackEndCarrera/Servicios/IService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BackEndCarrera.Dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace BackEndCarrera.Servicios
 {
-    interface IService
+    public interface IService
     {
+        Carrera BuscarCarreraPorId();
+        List<Asignatura> ObtenerAsignatura();
+        bool GuardarCarrera(Carrera oCarrera);
+        bool GuardarAsignatura(Asignatura oAsignatura);
+        bool EliminarCarrera(Carrera oCarrera);
+        bool EliminarDetalle(List<int> lst);
+        bool ActualizarCarrera(Carrera oCarrera);
+        bool ActualizarAsignatura(Asignatura oAsignatura);
     }
 }
