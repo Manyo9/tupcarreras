8fcbaab19f0355af93b9aee9bfdf72e1627fbdde
Regi <112874@tecnicatura.frc.utn.edu.ar>
2021-11-01 15:00:46 -0300

    detalles login

--- VSCarreras/Frontend/Frm_Login.Designer.cs
+++ VSCarreras/Frontend/Frm_Login.Designer.cs
@@ -38,14 +38,17 @@ namespace Frontend
             this.panel1 = new System.Windows.Forms.Panel();
             this.btnMinimized = new System.Windows.Forms.PictureBox();
             this.btnClose = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.btnMinimized)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnClose)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // txtUsuario
             // 
-            this.txtUsuario.Location = new System.Drawing.Point(78, 134);
+            this.txtUsuario.Location = new System.Drawing.Point(77, 218);
             this.txtUsuario.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.txtUsuario.Name = "txtUsuario";
             this.txtUsuario.Size = new System.Drawing.Size(234, 23);
@@ -53,7 +56,7 @@ namespace Frontend
             // 
             // txtContraseña
             // 
-            this.txtContraseña.Location = new System.Drawing.Point(78, 221);
+            this.txtContraseña.Location = new System.Drawing.Point(77, 293);
             this.txtContraseña.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.txtContraseña.Name = "txtContraseña";
             this.txtContraseña.Size = new System.Drawing.Size(234, 23);
@@ -64,12 +67,12 @@ namespace Frontend
             this.btnCancelar.BackColor = System.Drawing.Color.Navy;
             this.btnCancelar.FlatAppearance.BorderSize = 0;
             this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCancelar.Font = new System.Drawing.Font("Gadugi", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCancelar.Font = new System.Drawing.Font("Gadugi", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnCancelar.ForeColor = System.Drawing.Color.White;
-            this.btnCancelar.Location = new System.Drawing.Point(131, 384);
+            this.btnCancelar.Location = new System.Drawing.Point(125, 466);
             this.btnCancelar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(136, 36);
+            this.btnCancelar.Size = new System.Drawing.Size(133, 31);
             this.btnCancelar.TabIndex = 5;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.UseVisualStyleBackColor = false;
@@ -77,22 +80,22 @@ namespace Frontend
             // lblUsuario
             // 
             this.lblUsuario.AutoSize = true;
-            this.lblUsuario.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblUsuario.Font = new System.Drawing.Font("Gadugi", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.lblUsuario.ForeColor = System.Drawing.Color.White;
-            this.lblUsuario.Location = new System.Drawing.Point(78, 112);
+            this.lblUsuario.Location = new System.Drawing.Point(77, 196);
             this.lblUsuario.Name = "lblUsuario";
-            this.lblUsuario.Size = new System.Drawing.Size(63, 19);
+            this.lblUsuario.Size = new System.Drawing.Size(52, 16);
             this.lblUsuario.TabIndex = 6;
             this.lblUsuario.Text = "Usuario";
             // 
             // lblContraseña
             // 
             this.lblContraseña.AutoSize = true;
-            this.lblContraseña.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblContraseña.Font = new System.Drawing.Font("Gadugi", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.lblContraseña.ForeColor = System.Drawing.Color.White;
-            this.lblContraseña.Location = new System.Drawing.Point(78, 199);
+            this.lblContraseña.Location = new System.Drawing.Point(77, 271);
             this.lblContraseña.Name = "lblContraseña";
-            this.lblContraseña.Size = new System.Drawing.Size(88, 19);
+            this.lblContraseña.Size = new System.Drawing.Size(73, 16);
             this.lblContraseña.TabIndex = 7;
             this.lblContraseña.Text = "Contraseña";
             // 
@@ -103,10 +106,10 @@ namespace Frontend
             this.btnIniciarSesion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnIniciarSesion.Font = new System.Drawing.Font("Gadugi", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnIniciarSesion.ForeColor = System.Drawing.Color.White;
-            this.btnIniciarSesion.Location = new System.Drawing.Point(78, 287);
+            this.btnIniciarSesion.Location = new System.Drawing.Point(77, 360);
             this.btnIniciarSesion.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btnIniciarSesion.Name = "btnIniciarSesion";
-            this.btnIniciarSesion.Size = new System.Drawing.Size(234, 50);
+            this.btnIniciarSesion.Size = new System.Drawing.Size(234, 44);
             this.btnIniciarSesion.TabIndex = 8;
             this.btnIniciarSesion.Text = "Iniciar Sesion";
             this.btnIniciarSesion.UseVisualStyleBackColor = false;
@@ -117,17 +120,17 @@ namespace Frontend
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(45)))));
             this.panel1.Controls.Add(this.btnMinimized);
             this.panel1.Controls.Add(this.btnClose);
-            this.panel1.Location = new System.Drawing.Point(0, 2);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(397, 37);
+            this.panel1.Size = new System.Drawing.Size(397, 36);
             this.panel1.TabIndex = 9;
             // 
             // btnMinimized
             // 
             this.btnMinimized.Image = global::Frontend.Properties.Resources.minimizar;
-            this.btnMinimized.Location = new System.Drawing.Point(308, 3);
+            this.btnMinimized.Location = new System.Drawing.Point(330, 12);
             this.btnMinimized.Name = "btnMinimized";
-            this.btnMinimized.Size = new System.Drawing.Size(34, 30);
+            this.btnMinimized.Size = new System.Drawing.Size(24, 25);
             this.btnMinimized.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.btnMinimized.TabIndex = 11;
             this.btnMinimized.TabStop = false;
@@ -136,20 +139,43 @@ namespace Frontend
             // btnClose
             // 
             this.btnClose.Image = global::Frontend.Properties.Resources.Pasted_20211101_130216_preview_rev_1;
-            this.btnClose.Location = new System.Drawing.Point(348, 3);
+            this.btnClose.Location = new System.Drawing.Point(360, 12);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(32, 31);
+            this.btnClose.Size = new System.Drawing.Size(24, 22);
             this.btnClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.btnClose.TabIndex = 10;
             this.btnClose.TabStop = false;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Frontend.Properties.Resources.logo_utn_blanco;
+            this.pictureBox1.Location = new System.Drawing.Point(77, 79);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(251, 61);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gadugi", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(77, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 21);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "ADMINISTRCIÓN";
             // 
             // Frm_Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.ClientSize = new System.Drawing.Size(392, 519);
+            this.ClientSize = new System.Drawing.Size(392, 561);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnIniciarSesion);
             this.Controls.Add(this.lblContraseña);
@@ -165,6 +191,7 @@ namespace Frontend
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.btnMinimized)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnClose)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -180,5 +207,7 @@ namespace Frontend
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox btnClose;
         private System.Windows.Forms.PictureBox btnMinimized;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- VSCarreras/Frontend/Properties/Resources.Designer.cs
+++ VSCarreras/Frontend/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace Frontend.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap logo_utn_blanco {
+            get {
+                object obj = ResourceManager.GetObject("logo utn blanco", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap minimizar {
             get {
                 object obj = ResourceManager.GetObject("minimizar", resourceCulture);
