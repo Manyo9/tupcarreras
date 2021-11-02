5a3c4d460410fc5b88d4404e0c80b53de678922f
Regi <112874@tecnicatura.frc.utn.edu.ar>
2021-11-02 12:45:26 -0300

    Form reporte front

--- VSCarreras/Frontend/Frm_Reporte.cs
+++ VSCarreras/Frontend/Frm_Reporte.cs
@@ -12,6 +12,15 @@ namespace Frontend
 {
     public partial class Frm_Reporte : Form
     {
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn ColMateria;
+        private DataGridViewTextBoxColumn ColMaterias;
+        private Label label1;
+        private Button btnReporte;
+        private Panel panel1;
+        private PictureBox btnCerrar;
+        private PictureBox btnMinimized;
+
         public Frm_Reporte()
         {
             InitializeComponent();
@@ -20,6 +29,130 @@ namespace Frontend
         private void Frm_Reporte_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ColMateria = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColMaterias = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnReporte = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnCerrar = new System.Windows.Forms.PictureBox();
+            this.btnMinimized = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimized)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColMateria,
+            this.ColMaterias});
+            this.dataGridView1.Location = new System.Drawing.Point(47, 133);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(424, 183);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // ColMateria
+            // 
+            this.ColMateria.HeaderText = "Carrera";
+            this.ColMateria.Name = "ColMateria";
+            this.ColMateria.ReadOnly = true;
+            this.ColMateria.Width = 200;
+            // 
+            // ColMaterias
+            // 
+            this.ColMaterias.HeaderText = "Cantidad de Materias";
+            this.ColMaterias.Name = "ColMaterias";
+            this.ColMaterias.ReadOnly = true;
+            this.ColMaterias.Width = 200;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Cantidad de Materias por Carrera";
+            // 
+            // btnReporte
+            // 
+            this.btnReporte.Location = new System.Drawing.Point(323, 68);
+            this.btnReporte.Name = "btnReporte";
+            this.btnReporte.Size = new System.Drawing.Size(148, 23);
+            this.btnReporte.TabIndex = 2;
+            this.btnReporte.Text = "VER REPORTE";
+            this.btnReporte.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.btnCerrar);
+            this.panel1.Controls.Add(this.btnMinimized);
+            this.panel1.Location = new System.Drawing.Point(0, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(543, 35);
+            this.panel1.TabIndex = 3;
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Image = global::Frontend.Properties.Resources.Pasted_20211101_130216_preview_rev_1;
+            this.btnCerrar.Location = new System.Drawing.Point(490, 11);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(24, 24);
+            this.btnCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnCerrar.TabIndex = 5;
+            this.btnCerrar.TabStop = false;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // btnMinimized
+            // 
+            this.btnMinimized.Image = global::Frontend.Properties.Resources.minimizar;
+            this.btnMinimized.Location = new System.Drawing.Point(460, 11);
+            this.btnMinimized.Name = "btnMinimized";
+            this.btnMinimized.Size = new System.Drawing.Size(24, 24);
+            this.btnMinimized.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnMinimized.TabIndex = 4;
+            this.btnMinimized.TabStop = false;
+            this.btnMinimized.Click += new System.EventHandler(this.btnMinimized_Click);
+            // 
+            // Frm_Reporte
+            // 
+            this.ClientSize = new System.Drawing.Size(521, 404);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnReporte);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Frm_Reporte";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimized)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void btnMinimized_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
         }
     }
 }
